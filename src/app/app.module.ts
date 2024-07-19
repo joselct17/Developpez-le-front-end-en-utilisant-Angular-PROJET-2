ddce6e91f2bf325d518600893209a33da14ebcd8
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-07-19 09:59:24 +0200

    Add DashboardComponent to AppModule
    DashboardComponent has been added to the declarations array in AppModule. This now includes the DashboardComponent as part of the root AppModule, making it available throughout the entire application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 
+//import modules pour les afficher
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, DashboardComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
