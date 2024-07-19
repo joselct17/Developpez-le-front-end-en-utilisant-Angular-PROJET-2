b8787adaf0527531cb5a97c8682763b25324a522
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-07-19 09:58:45 +0200

    Refactor Olympic service to use OlympicData model
    The code has been refactored to utilize the OlympicData model within the Olympic service. This eliminates the use of 'any' by replacing it with explicit 'OlympicData[]' in 'olympics$' BehaviorSubject and the http.get call. In case of errors, the service now returns an empty array instead of null.

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,26 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, of} from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+//import de OlympicData pour le rajouter dans le type de olympics$ pour eviter les any
+import { OlympicData } from 'src/app/core/models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  //rajout de OlympicData[]
+  private olympics$ = new BehaviorSubject<OlympicData[]>([]);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<OlympicData[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
         // TODO: improve error handling
         console.error(error);
         // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
-        return caught;
+        this.olympics$.next([]);
+        return of([]);
       })
     );
   }
