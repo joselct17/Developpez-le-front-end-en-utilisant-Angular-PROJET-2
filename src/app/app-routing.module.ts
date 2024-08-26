dba8e9f83b032cf01a8eae080c88fd9c375b95de
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-08-26 16:04:03 +0200

    Simplify routes and add DetailComponent
    Consolidated redundant routes and added a new route for DetailComponent. This reduces potential conflicts and streamlines navigation within the app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import {DetailComponent} from "./pages/detail/detail.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    component: DashboardComponent,
-  },
-
-  {
-    path: '', redirectTo:'/dashboard', pathMatch:'full'
-
-  },
-  {
-    path:'dashboard', component:DashboardComponent
-  },
-  {
-    path: '**', // wildcard
-    component: NotFoundComponent,
-  },
-
+  {path: '', redirectTo:'/dashboard', pathMatch:'full'},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'details/:id', component:DetailComponent},
+  {path: '**', component: NotFoundComponent,},
 ];
 
 @NgModule({
