c213fa07d387d3cb408d20c969a3c904c50aae36
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-08-26 16:04:26 +0200

    Add navigation to detail view on chart click.
    Added Angular Router import and injected Router into DashboardComponent. Implemented click event on the chart to navigate to the detail view of the selected country using the country's ID.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,83 +3,62 @@ import { Subscription } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { OlympicData } from 'src/app/core/models/Olympic';
 import { Chart, ChartConfiguration, ChartData, registerables } from 'chart.js';
-
+import { Router } from '@angular/router';  // Assurez-vous d'importer Router
 
 Chart.register(...registerables);
-// J'enregistre tous les composants nécessaires de Chart.js
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-// Je définis le décorateur @Component pour le composant DashboardComponent
-
 export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
   olympicData: OlympicData[] = [];
-  // Je déclare une propriété olympicData pour stocker les données olympiques
-
   private subscriptions: Subscription = new Subscription();
-  // J'initialise une propriété subscriptions pour gérer les abonnements aux Observables
-
   private chart: Chart | undefined;
-  // Je déclare une propriété chart pour stocker l'instance du graphique Chart.js
 
-  constructor(private olympicService: OlympicService) { }
-  // Je définis un constructeur qui injecte le service OlympicService
+  constructor(
+    private olympicService: OlympicService,
+    private router: Router  // Injectez Router ici
+  ) { }
 
   ngOnInit() {
-    // Méthode appelée lors de l'initialisation du composant
     this.subscriptions.add(
-      //appel observable olympicService.loadInitialData()
       this.olympicService.loadInitialData().subscribe()
-      // J'ajoute un abonnement pour charger les données initiales à partir du service OlympicService
     );
 
     this.subscriptions.add(
-      //appel observable olympicService.getOlympics()
       this.olympicService.getOlympics().subscribe(data => {
         this.olympicData = data;
-        // Je mets à jour la propriété olympicData avec les données reçues
         this.createChart();
-        // Je crée le graphique avec les nouvelles données
       })
     );
   }
 
   ngAfterViewInit() {
-    // Méthode appelée après l'initialisation de la vue du composant
     if (this.olympicData.length) {
       this.createChart();
-      // Je crée le graphique si des données olympiques sont disponibles
     }
   }
 
   createChart() {
-    // Méthode pour créer le graphique
     const chartElement = document.getElementById('medalsChart') as HTMLCanvasElement;
-    // Je récupère l'élément canvas pour le graphique
     if (!chartElement) {
       return;
-      // Si l'élément canvas n'existe pas, je quitte la méthode
     }
 
     if (this.chart) {
       this.chart.destroy();
-      // Si un graphique existe déjà, je le détruis avant de créer un nouveau
     }
 
     const chartData: ChartData<'pie', number[], string> = {
       labels: this.olympicData.map(item => item.country),
-      // Je crée les labels pour le graphique à partir des pays dans les données olympiques
       datasets: [{
         label: 'Total Medals',
         data: this.olympicData.map(item =>
           item.participations.reduce((acc, participation) => acc + participation.medalsCount, 0)
         ),
-        // Je calcule le nombre total de médailles pour chaque pays
-        backgroundColor: ['red', 'blue', 'green', 'yellow', 'purple']
-        // Je définis les couleurs de fond pour les segments du graphique
+        backgroundColor: ['#7C3D52', '#86A1D8', '#B6CBE5', '#BCE0F0', '#986066']
       }]
     };
 
@@ -98,22 +77,26 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
         animation: {
           animateScale: true,
           animateRotate: true
+        },
+        onClick: (event: any, elements: string | any[]) => {
+          if (elements.length > 0) {
+            const index = elements[0].index;
+            const selectedCountry = this.olympicData[index];
+            if (selectedCountry) {
+              this.router.navigate(['/details', selectedCountry.id]);  // Utilisez this.router ici pour la navigation
+            }
+          }
         }
       } as any
-      // Je force le type pour correspondre avec les données attendues
     };
 
     this.chart = new Chart(chartElement, chartConfig as any);
-    // Je crée une nouvelle instance de Chart avec la configuration définie
   }
 
   ngOnDestroy() {
-    // Méthode appelée lors de la destruction du composant
     this.subscriptions.unsubscribe();
-    // Je me désabonne de tous les abonnements pour éviter les fuites de mémoire
     if (this.chart) {
       this.chart.destroy();
-      // Si un graphique existe, je le détruis pour libérer les ressources
     }
   }
 }
